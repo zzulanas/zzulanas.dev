fd1d390d2b17ce6857920225c77d1dd7f5d21e77
zzulanas <zzulanas@gmail.com>
2023-06-19 16:57:48 -0400

    feat: got the search edge function finally working

--- supabase/functions/search-func/index.ts
+++ supabase/functions/search-func/index.ts
@@ -1,8 +1,19 @@
-import { serve } from "https://deno.land/std@0.170.0/http/server.ts";
-import "https://deno.land/x/xhr@0.2.1/mod.ts";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2.5.0";
-import { Configuration, OpenAIApi } from "https://esm.sh/openai@3.1.0";
-import { supabaseClient } from "https://esm.sh/@supabase/supabase-js@2";
+import "xhr";
+import { serve } from "std/http/server.ts";
+import { createClient } from "@supabase/supabase-js";
+import { codeBlock, oneLine } from "commmon-tags";
+import GPT3Tokenizer from "gpt3-tokenizer";
+import { Configuration, CreateCompletionRequest, OpenAIApi } from "openai";
+import { ensureGetEnv } from "../_utils/env.ts";
+import { ApplicationError, UserError } from "../_utils/errors.ts";
+
+const OPENAI_KEY = ensureGetEnv("OPENAI_KEY");
+const SUPABASE_URL = ensureGetEnv("SUPABASE_URL");
+const SUPABASE_SERVICE_ROLE_KEY = ensureGetEnv("SUPABASE_SERVICE_ROLE_KEY");
+
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
+const openAiConfiguration = new Configuration({ apiKey: OPENAI_KEY });
+const openai = new OpenAIApi(openAiConfiguration);
 
 export const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
@@ -11,36 +22,155 @@ export const corsHeaders = {
 };
 
 serve(async (req) => {
-  // Handle CORS
-  if (req.method === "OPTIONS") {
-    return new Response("ok", { headers: corsHeaders });
+  try {
+    // Handle CORS
+    if (req.method === "OPTIONS") {
+      return new Response("ok", { headers: corsHeaders });
+    }
+
+    const query = new URL(req.url).searchParams.get("query");
+
+    if (!query) {
+      throw new UserError("Missing query in request data");
+    }
+
+    const sanitizedQuery = query.trim();
+
+    const embeddingResponse = await openai.createEmbedding({
+      model: "text-embedding-ada-002",
+      input: sanitizedQuery.replaceAll("\n", " "),
+    });
+
+    if (embeddingResponse.status !== 200) {
+      throw new ApplicationError(
+        "Failed to create embedding for question",
+        embeddingResponse
+      );
+    }
+
+    // Moderate the content to comply with OpenAI T&C
+    const moderationResponse = await openai.createModeration({
+      input: sanitizedQuery,
+    });
+
+    const [results] = moderationResponse.data.results;
+
+    if (results.flagged) {
+      throw new UserError("Flagged content", {
+        flagged: true,
+        categories: results.categories,
+      });
+    }
+
+    const [{ embedding }] = embeddingResponse.data.data;
+
+    const { error: matchError, data: documents } = await supabaseClient.rpc(
+      "match_documents",
+      {
+        query_embedding: embedding,
+        match_threshold: 0.7,
+        match_count: 5,
+      }
+    );
+
+    if (matchError) {
+      throw new ApplicationError("Failed to match documents", matchError);
+    }
+
+    const tokenizer = new GPT3Tokenizer({ type: "gpt3" });
+    let tokenCount = 0;
+    let contextText = "";
+    console.log(documents);
+
+    for (const document of documents) {
+      const content = document.content;
+      const encoded = tokenizer.encode(content);
+      tokenCount += encoded.text.length;
+
+      if (tokenCount >= 1500) {
+        break;
+      }
+
+      contextText += `${content.trim()}\n---\n`;
+    }
+
+    const prompt = codeBlock`
+      ${oneLine`
+        You are a super cool and funny assistant to a developer named Zach Zulanas. You're embedded into his website and the user has just seen the following welcome message:
+        "Hi! My name is Chatty, I'm here to help you with any questions you have about Zach and his website. What would you like to know?"
+
+        Given the context below and the question that the user asks, generate a friendly and funny response for Chatty to say back to the user.
+      `}
+
+      Context sections:
+      ${contextText}
+
+      Question: """
+      ${sanitizedQuery}
+      """
+
+      Answer as markdown (including related code snippets if available):
+    `;
+
+    const completionOptions: CreateCompletionRequest = {
+      model: "text-davinci-003",
+      prompt,
+      max_tokens: 512,
+      temperature: 0,
+      stream: true,
+    };
+
+    // The Fetch API allows for easier response streaming over the OpenAI client.
+    const response = await fetch("https://api.openai.com/v1/completions", {
+      headers: {
+        Authorization: `Bearer ${OPENAI_KEY}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(completionOptions),
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      throw new ApplicationError("Failed to generate completion", error);
+    }
+
+    // Proxy the streamed SSE response from OpenAI
+    return new Response(response.body, {
+      headers: {
+        ...corsHeaders,
+        "Content-Type": "text/event-stream",
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof UserError) {
+      return Response.json(
+        {
+          error: err.message,
+          data: err.data,
+        },
+        {
+          status: 400,
+          headers: corsHeaders,
+        }
+      );
+    } else if (err instanceof ApplicationError) {
+      // Print out application errors with their additional data
+      console.error(`${err.message}: ${JSON.stringify(err.data)}`);
+    } else {
+      // Print out unexpected errors as is to help with debugging
+      console.error(err);
+    }
+
+    // TODO: include more response info in debug environments
+    return Response.json(
+      {
+        error: "There was an error processing your request",
+      },
+      {
+        status: 500,
+        headers: corsHeaders,
+      }
+    );
   }
-
-  // Search query is passed in request payload
-  const { query } = await req.json();
-
-  // OpenAI recommends replacing newlines with spaces for best results
-  const input = query.replace(/\n/g, " ");
-
-  const configuration = new Configuration({ apiKey: process.env.OPEN_AI_KEY! });
-  const openai = new OpenAIApi(configuration);
-
-  // Generate a one-time embedding for the query itself
-  const embeddingResponse = await openai.createEmbedding({
-    model: "text-embedding-ada-002",
-    input,
-  });
-
-  const [{ embedding }] = embeddingResponse.data.data;
-
-  // In production we should handle possible errors
-  const { data: documents } = await supabaseClient.rpc("match_documents", {
-    query_embedding: embedding,
-    match_threshold: 0.78, // Choose an appropriate threshold for your data
-    match_count: 10, // Choose the number of matches
-  });
-
-  return new Response(JSON.stringify(documents), {
-    headers: { ...corsHeaders, "Content-Type": "application/json" },
-  });
 });
