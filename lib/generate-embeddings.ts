516c47205d2e47b38c4d447c86ffc5b3a44a7270
zzulanas <zzulanas@gmail.com>
2023-06-19 23:09:14 -0400

    feat: fixed some config stuff

---
+++ lib/generate-embeddings.ts
@@ -0,0 +1,34 @@
+import { Configuration, OpenAIApi } from "openai";
+import { supabaseClient } from "./supabase-client";
+
+const configuration = new Configuration({ apiKey: process.env.OPENAI_KEY });
+const openAi = new OpenAIApi(configuration);
+
+const documents = [
+  "Zach loves long walks on the beach",
+  "Zach loves ice cream, especially vanilla ice cream",
+  "In Zach's free time he likes to rock climb, the highest grade he can do is around a V5, he's still learning!",
+];
+
+async function generateEmbeddings(documents: string[]) {
+  // Assuming each document is a string
+  for (const document of documents) {
+    // OpenAI recommends replacing newlines with spaces for best results
+    const input = document.replace(/\n/g, " ");
+
+    const embeddingResponse = await openAi.createEmbedding({
+      model: "text-embedding-ada-002",
+      input,
+    });
+
+    const [{ embedding }] = embeddingResponse.data.data;
+
+    // In production we should handle possible errors
+    await supabaseClient.from("documents").insert({
+      content: document,
+      embedding,
+    });
+  }
+}
+
+generateEmbeddings(documents);
