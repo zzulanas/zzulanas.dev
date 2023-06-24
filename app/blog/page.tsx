4ac72e1a177bfe5b70cd8cbe963a5c610c73e510
zzulanas <zzulanas@gmail.com>
2023-06-24 16:59:41 -0400

    blog kinda working but a lil broken, need to make work with dynamic slugs

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,22 @@
-export default function Blog() {
-  return <h1>blog</h1>;
+import MoreStories from "@/components/post/more-posts";
+import type PostType from "@/interfaces/post";
+import { getAllPosts } from "@/lib/api";
+
+export default async function Blog() {
+  const allPosts: PostType[] = await getPosts();
+  return <MoreStories posts={allPosts} />;
+}
+
+async function getPosts(): Promise<PostType[]> {
+  const allPosts = await getAllPosts([
+    "title",
+    "date",
+    "slug",
+    "author",
+    "coverImage",
+    "excerpt",
+  ]);
+
+  //@ts-ignore
+  return allPosts;
 }
