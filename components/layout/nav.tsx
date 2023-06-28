a24f94c140878e68e47a64ec6e9384fe2d5064da
zzulanas <zzulanas@gmail.com>
2023-06-28 19:40:24 -0400

    fix: small nav state stuff

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,21 +1,49 @@
 "use client";
 
 import Link from "next/link";
-import { LayoutGroup, motion } from "framer-motion";
+import { LayoutGroup, motion, useAnimation } from "framer-motion";
 import { riseWithFade, staggerChildren } from "@/utils/animations";
 import AnimatedText from "../AnimatedText";
-import { usePathname } from "next/navigation";
-import MenuButton from "../ui/MenuButton";
+import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 
 // TODO: figure out how to get staggered animation for zzulanas.dev and rest of nav
 
 export default function Nav() {
+  const path01Controls = useAnimation();
+  const path02Controls = useAnimation();
+  const path01Variants = {
+    open: { d: "M3.06061 2.99999L21.0606 21" },
+    closed: { d: "M0 9.5L24 9.5" },
+  };
+
+  const path02Variants = {
+    open: { d: "M3.00006 21.0607L21 3.06064" },
+    moving: { d: "M0 14.5L24 14.5" },
+    closed: { d: "M0 14.5L15 14.5" },
+  };
   const pathname = usePathname();
   const [isOpen, setOpen] = useState(false);
+  const router = useRouter();
   const isAboutSelected = pathname === "/about";
   const isProjectsSelected = pathname === "/projects";
   const isBlogSelected = pathname === "/blog";
+
+  const onButtonClick = async () => {
+    // change the internal state
+    setOpen(!isOpen);
+
+    // start animation
+    if (!isOpen) {
+      await path02Controls.start(path02Variants.moving);
+      path01Controls.start(path01Variants.open);
+      path02Controls.start(path02Variants.open);
+    } else {
+      path01Controls.start(path01Variants.closed);
+      await path02Controls.start(path02Variants.moving);
+      path02Controls.start(path02Variants.closed);
+    }
+  };
   return (
     <LayoutGroup>
       <div
@@ -71,8 +99,23 @@ export default function Nav() {
               />
             </Link>
           </motion.div>
-          <div className="md:hidden" onClick={() => setOpen(!isOpen)}>
-            <MenuButton />
+          <div className="md:hidden">
+            <button onClick={onButtonClick}>
+              <svg width="24" height="24" viewBox="0 0 24 24">
+                <motion.path
+                  {...path01Variants.closed}
+                  animate={path01Controls}
+                  transition={{ duration: 0.2 }}
+                  stroke="#000"
+                />
+                <motion.path
+                  {...path02Variants.closed}
+                  animate={path02Controls}
+                  transition={{ duration: 0.2 }}
+                  stroke="#000"
+                />
+              </svg>
+            </button>
           </div>
         </motion.div>
         {isOpen && (
@@ -83,7 +126,7 @@ export default function Nav() {
             <Link
               href="/about"
               className="group text-sky-600 transition duration-100"
-              onClick={() => setOpen(!isOpen)}
+              onClick={onButtonClick}
             >
               <AnimatedText
                 title="about"
@@ -96,7 +139,7 @@ export default function Nav() {
             <Link
               href="/projects"
               className="group text-sky-600 transition duration-100"
-              onClick={() => setOpen(!isOpen)}
+              onClick={onButtonClick}
             >
               <AnimatedText
                 title="projects"
@@ -109,7 +152,7 @@ export default function Nav() {
             <Link
               href="/blog"
               className="group text-sky-600 transition duration-100"
-              onClick={() => setOpen(!isOpen)}
+              onClick={onButtonClick}
             >
               <AnimatedText
                 title="blog"
